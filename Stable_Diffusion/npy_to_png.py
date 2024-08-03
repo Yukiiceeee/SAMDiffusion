3cc125f71e1910d81936d69fd0afbf0e6e567543
jackefn <1901093340@qq.com>
2024-08-03 10:21:01 +0800

    bench transfer npy to img

--- Stable_Diffusion/npy_to_png.py
+++ Stable_Diffusion/npy_to_png.py
@@ -1,8 +1,10 @@
 import numpy as np
 from PIL import Image
 import os
+import time
 
-def npy_to_png(npy_file, output_dir):
+
+def npy_to_png(npy_file, output_dir,img_cnt=200001,file_name=""):
     # 加载 .npy 文件，并允许使用 pickle
     data = np.load(npy_file, allow_pickle=True).item()
     
@@ -20,6 +22,11 @@ def npy_to_png(npy_file, output_dir):
         print(f"Processing key: {key}")
         print(f"Image data shape: {image_data.shape}")
         print(f"Image data dtype: {image_data.dtype}")
+
+        # 如果 image_data 全为零，则跳过
+        if np.all(image_data == 0):
+            print(f"Skipping key {key} because the image data is all zeros.")
+            continue
 
         # 如果数据不是 uint8 类型（0-255），则需要归一化
         if image_data.dtype != np.uint8:
@@ -41,13 +48,23 @@ def npy_to_png(npy_file, output_dir):
             raise ValueError("Unsupported image shape")
 
         # 构造输出文件路径
-        output_file = os.path.join(output_dir, f"{key}_1.png")
+        # img_cnt = img_cnt + 1
+        output_file = os.path.join(output_dir, f"{file_name}.png")
 
         # 保存图像为 .png 文件
         img.save(output_file)
         print(f"Saved {output_file}")
 
+def process_all_npy_files(input_dir, output_dir):
+    img_cnt = 200000
+    for file_name in os.listdir(input_dir):
+        if file_name.endswith('.npy'):
+            # print(file_name)
+            # time.sleep(5)
+            img_cnt = img_cnt + 1
+            npy_file = os.path.join(input_dir, file_name)
+            npy_to_png(npy_file, output_dir,img_cnt,file_name)
 
-npy_file = '/home/xiangchao/home/muxinyu/SAMDiffusion/DiffMask_VOC/VOC_Multi_Attention_cat_sub_1000_NoClipRetrieval_sample/npy/image_cat_200001.npy'  
-output_dir = '/home/xiangchao/home/muxinyu/SAMDiffusion/DiffMask_VOC/VOC_Multi_Attention_cat_sub_1000_NoClipRetrieval_sample'  
-npy_to_png(npy_file, output_dir)
+input_dir = '/data2/mxy/SAMDiffusion/DiffMask_VOC/VOC_Multi_Attention_cat_sub_1000_NoClipRetrieval_sample/npy'
+output_dir = '/data2/mxy/SAMDiffusion/DiffMask_VOC/VOC_Multi_Attention_cat_sub_1000_NoClipRetrieval_sample/mask'
+process_all_npy_files(input_dir, output_dir)
